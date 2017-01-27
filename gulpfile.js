0b718ca5d48ddea807f475771b14286b8ccffa9f
Eivind Liland <spookysys@gmail.com>
2017-01-27 01:19:21 +0100

    Better folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,12 @@
 // Ref. https://www.sitepoint.com/introduction-gulp-js/
 // Reg. http://brianflove.com/2016/11/08/typescript-2-express-node/
 
-/*
-├── bin
-│   └── www
-├── dist
-│   ├── interfaces
-│   │   └── user.js
-│   ├── models
-│   │   ├── model.js
-│   │   └── user.js
-│   ├── routes
-│   │   ├── index.js
-│   │   └── route.js
-│   ├── schemas
-│   │   └── user.js
-│   ├── server.js
-│   ├── test
-│   │   └── user.js
-│   └── views
-│       └── index.pug
-├── gruntfile.js
-├── package.json
-├── public
-├── src
-│   ├── interfaces
-│   │   └── user.ts
-│   ├── models
-│   │   ├── model.ts
-│   │   └── user.ts
-│   ├── npm-debug.log
-│   ├── routes
-│   │   ├── index.ts
-│   │   └── route.ts
-│   ├── schemas
-│   │   └── user.ts
-│   ├── server.ts
-│   └── test
-│       └── user.ts
-└── views
-    └── index.pug
-*/
-
 var
 	// modules
 	gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	newer = require('gulp-newer'),
-	prune = require('gulp-prune'),
+	//prune = require('gulp-prune'),
 	imagemin = require('gulp-imagemin'),
 	htmlclean = require('gulp-htmlclean'),
 	//	concat = require('gulp-concat'),
@@ -77,24 +36,45 @@ devBuild = (process.env.NODE_ENV !== 'production'),
 var fileCache = new FileCache();
 
 
-// // image processing
-// gulp.task('images', function () {
-// 	var out = folder.dist + 'images/';
-// 	return gulp.src(folder.src + 'images/**/*')
-// 		.pipe(newer(out))
-// 		.pipe(imagemin({ optimizationLevel: 5 }))
-// 		.pipe(gulp.dest(out));
-// });
+
+// Image processing
+gulp.task('images', function () {
+	var out = folder.dist + 'images/';
+	return gulp.src(folder.src + 'images/**/*')
+		.pipe(newer(out))
+		.pipe(imagemin({ optimizationLevel: 5 }))
+		.pipe(gulp.dest(out));
+});
 
 
-// // HTML processing
-// gulp.task('html', ['images'], function () {
-// 	var out = folder.dist + 'html/';
-// 	return gulp.src(folder.src + 'html/**/*')
-// 		.pipe(newer(out))
-// 		.pipe(devBuild ? gutil.noop() : htmlclean())
-// 		.pipe(gulp.dest(out));
-// });
+// HTML processing
+gulp.task('html', ['images'], function () {
+	var out = folder.dist + 'html/';
+	return gulp.src(folder.src + 'html/**/*')
+		.pipe(newer(out))
+		.pipe(devBuild ? gutil.noop() : htmlclean())
+		.pipe(gulp.dest(out));
+});
+
+
+// CSS processing
+gulp.task('css', ['images'], function () {
+	var out = folder.dist + 'css/';
+	return gulp.src(folder.src + 'scss/main.scss')
+		.pipe(sass({
+			outputStyle: 'nested',
+			imagePath: 'images/',
+			precision: 3,
+			errLogToConsole: true
+		}))
+		.pipe(postcss([
+			assets({ loadPaths: ['images/'] }),
+			autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+			mqpacker,
+			cssnano
+		]))
+		.pipe(gulp.dest(out));
+});
 
 
 // JavaScript processing
@@ -115,21 +95,37 @@ var fileCache = new FileCache();
 // Copy
 gulp.task('copy', function () {
 	var out = folder.dist;
-	return gulp.src(['./public/**', './views/**'], { "base": "." })
-		//.pipe(prune(out))
+	return gulp.src([folder.src + '**/*.pug'], { "base": folder.src })
 		.pipe(newer(out))
 		.pipe(gulp.dest(out))
 });
+
+
+// Javascript
+gulp.task('js', function () {
+	var out = folder.dist;
+	return gulp.src(folder.src + '**/*.js')
+		.pipe(newer(out))
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.', {
+			includeContent: false,
+			destPath: out,
+			sourceRoot: '../' + folder.src
+		}))
+		.pipe(gulp.dest(out))
+});
+
 
 // TypeScript processing
 var tsProject = ts.createProject("tsconfig.json");
 gulp.task('ts', function () {
 	var out = folder.dist;
 	var stream = gulp.src(folder.src + '**/*.ts')
-		//.pipe(prune(out))
 		.pipe(newer(out))
 		.pipe(sourcemaps.init())
 		.pipe(tsProject())
+		.pipe(uglify())
 		.pipe(sourcemaps.write('.', {
 			includeContent: false,
 			destPath: out,
@@ -140,29 +136,10 @@ gulp.task('ts', function () {
 });
 
 
-// // CSS processing
-// gulp.task('css', ['images'], function () {
-// 	var out = folder.dist + 'css/';
-// 	return gulp.src(folder.src + 'scss/main.scss')
-// 		.pipe(sass({
-// 			outputStyle: 'nested',
-// 			imagePath: 'images/',
-// 			precision: 3,
-// 			errLogToConsole: true
-// 		}))
-// 		.pipe(postcss([
-// 			assets({ loadPaths: ['images/'] }),
-// 			autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-// 			mqpacker,
-// 			cssnano
-// 		]))
-// 		.pipe(gulp.dest(out));
-// });
-
 
 
 // run all tasks
-gulp.task('real_build', ['copy', 'ts']);
+gulp.task('real_build', ['copy', 'ts', 'js']);
 
 
 
